8ae1c274995b97b2f53c11e4a0eb9d4c5416b229
Anshu <utkarshpateriya5@gmail.com>
2026-02-21 04:36:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,4 +47,14 @@ window.onclick = function(e){
         closeModal();
     }
 }
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("menu-toggle");
+    const navbar = document.getElementById("navbar");
+
+    toggle.addEventListener("click", function () {
+        navbar.classList.toggle("active");
+    });
+});
+</script>
 
